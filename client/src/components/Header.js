fe1dc6f38992a8a444502306b0a521fedb873dbf
zarina <iman19230197@gmail.com>
2025-03-14 15:13:43 +0000

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,15 +10,15 @@ function Header() {
           
           <nav className="hidden md:block">
             <ul className="flex space-x-6">
-              <li><Link to="/" className="hover:text-blue-200">الرئيسية</Link></li>
-              <li><Link to="/search" className="hover:text-blue-200">بحث الإعلانات</Link></li>
-              <li><Link to="/patterns" className="hover:text-blue-200">أنماط النجاح</Link></li>
+              <li><Link to="/" className="hover:text-blue-200">Home</Link></li>
+              <li><Link to="/search" className="hover:text-blue-200">Ad Search</Link></li>
+              <li><Link to="/patterns" className="hover:text-blue-200">Success Patterns</Link></li>
             </ul>
           </nav>
           
           <div>
             <Link to="/search" className="bg-white text-blue-700 hover:bg-blue-100 px-4 py-2 rounded-lg font-medium">
-              ابدأ البحث
+              Start Search
             </Link>
           </div>
         </div>
